4b7e8a00fcb9117bcadf6d6c6fe77ff19a689ae0
Lakkireddy Sunandana <sunandanalakkireddy@gmail.com>
2025-05-15 00:17:15 +0530

    Updating

--- client/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -129,7 +129,7 @@ const MessageContainer = () => {
     };
 
     const renderChannelMessages = (message) => {
-        return (<div className={`mt-5 ${message.sender_id !== userInfo._id? "items-start" : "items-end"}`}>
+        return (<div className={`mt-5 ${message.sender._id !== userInfo._id? "text-left" : "text-right"}`}>
             { message.messageType === "text" && 
                 (<div className={`${message.sender_id === userInfo.id ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50" : "bg-[#2a2b33]/5 text-white/80 border-[#ffffff]/20"} border inline-block p-4 rounded my-1 max-w-[50%] break-words ml-9`}>
                     {message.content}
