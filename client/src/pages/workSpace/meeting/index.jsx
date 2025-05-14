8c91c4a125543f32cfd20926eed50afd49299c1c
Lakkireddy Sunandana <sujithreddy648@gmail.com>
2025-05-14 22:23:27 +0530

    Minor fixes

--- client/src/pages/workSpace/meeting/index.jsx
+++ client/src/pages/workSpace/meeting/index.jsx
@@ -16,8 +16,8 @@ import io from 'socket.io-client';
 const Meetings = () => {
 
     const {userInfo,setUserInfo} = useAppStore();
-    const [isAudioOn, setAudioOn] = useState(false);
-    const [isVideoOn, setVideoOn] = useState(false);
+    const [isAudioOn, setAudioOn] = useState(true);
+    const [isVideoOn, setVideoOn] = useState(true);
     const [localStream,setLocalStream] = useState(null);
     const [remoteStream,setRemoteStream]=useState(null);
     const [peerConnection,setPeerConnection]=useState(null);
@@ -28,7 +28,17 @@ const Meetings = () => {
     let mediaio=null;
 
     const handleLeaveClick = async ()=>{
-        
+        if(peerConnection){
+                peerConnection.close();
+                peerConnection.onicecandidate = null
+                peerConnection.onaddstream = null
+                setPeerConnection(null);
+
+            //set both video tags to empty
+            localVideoRef.current.srcObject = null;
+            remoteVideoRef.current.srcObject = null;
+        }
+        navigate("/workspace");
     
     };
 
@@ -40,6 +50,11 @@ const Meetings = () => {
     const handleVideoClick = async ()=>{
         //Add more functionality about video on and off
         setVideoOn(!isVideoOn);
+        if(isVideoOn){
+            localVideoRef.current.srcObject = null;
+        }else{
+            localVideoRef.current.srcObject=localStream;
+        }
     };
     useEffect(()=>{
         setMedia().then(()=>{
